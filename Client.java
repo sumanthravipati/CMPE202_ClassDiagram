a20400dc9304bba2f495363fd0b5d67e0a85e959
sumanthravipati <43222215+sumanthravipati@users.noreply.github.com>
2019-02-25 19:53:24 -0800

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,9 @@
+
+
+public class Client {
+	
+ public static void runTest(){
+	 Component theOrder = BuildOrder.getOrder() ;
+	 theOrder.printDescription();
+ 	}
+}
---
+++ PriceDecorator.java
@@ -0,0 +1,4 @@
+public interface PriceDecorator
+{
+ Double getPrice();
+}
